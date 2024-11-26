0c2f3fc3aef7b88da4c50010067e99b83806526d
CaeHill <hillcaetano@gmail.com>
2024-11-26 16:53:43 -0300

    Add interfaces

--- dados/Transporte.java
+++ dados/Transporte.java
@@ -1,25 +1,51 @@
 package dados;
 
-public abstract class Transporte {
-
+public abstract class   Transporte {
 	private int numero;
-
 	private String nomeCliente;
-
 	private String descricao;
-
 	private double peso;
+	private double latOrig;
+	private double longOrig;
+	private double latDest;
+	private double longDest;
+	private String status; // PENDENTE, ALOCADO, TERMINADO, CANCELADO
+	private double custoFinal;
 
-	private double latitudeOrigem;
+	public Transporte(int numero, String nomeCliente, String descricao, double peso,
+					  double latOrig, double longOrig, double latDest, double longDest) {
+		this.numero = numero;
+		this.nomeCliente = nomeCliente;
+		this.descricao = descricao;
+		this.peso = peso;
+		this.latOrig = latOrig;
+		this.longOrig = longOrig;
+		this.latDest = latDest;
+		this.longDest = longDest;
+	}
 
-	private double latitudeDestino;
+	public Transporte() {
 
-	private double longitudeOrigem;
+	}
 
-	private double longitudeDestino;
 
-	private Estado situacao;
+	public abstract void calcularCusto();
 
-	public abstract double calculaCusto();
+	public void alterarStatus(String novoStatus) {
+		if (!status.equals("TERMINADO") && !status.equals("CANCELADO")) {
+			this.status = novoStatus;
+		}
+	}
 
+	// Getters e setters
+
+	public int getNumero() { return numero; }
+	public String getNomeCliente() { return nomeCliente; }
+	public String getDescricao() { return descricao; }
+	public double getLatOrig() { return latOrig; }
+	public double getLongOrig() { return longOrig; }
+	public double getLatDest() { return latDest; }
+	public double getLongDest() { return longDest; }
+	public double getCustoFinal() { return custoFinal; }
+	public void setCustoFinal(double custoFinal) { this.custoFinal = custoFinal; }
 }
