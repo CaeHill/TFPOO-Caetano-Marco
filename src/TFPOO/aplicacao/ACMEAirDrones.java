f09700e4bbaeddb2161c82b9a90c10f23f854e68
CaeHill <hillcaetano@gmail.com>
2024-11-27 23:40:30 -0300

    rodar pela main

--- src/TFPOO/aplicacao/ACMEAirDrones.java
+++ src/TFPOO/aplicacao/ACMEAirDrones.java
@@ -1,10 +1,11 @@
 package TFPOO.aplicacao;
 
-import TFPOO.gestores.*;
+import TFPOO.interfaces.Menu;
+import javafx.application.Application;
 
 public class ACMEAirDrones {
 
 	public void executar() {
-
+		Application.launch(Menu.class);
 	}
 }
